db86eb5bf10c4a567fed56c35c66f6b8bcb44e85
igorfcfs <igorfcfs@gmail.com>
2022-03-15 15:23:41 -0300

    Distribuicao do seu Codigo

--- bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/modelo/Conta.java
@@ -1,5 +1,11 @@
 package br.com.bytebank.banco.modelo;
 
+/**
+ * Classe representa a moldura de uma conta.
+ * 
+ * @author Igor Casita
+ *
+ */
 public abstract class Conta {
 	protected double saldo;
 	private int agencia;
@@ -7,6 +13,12 @@ public abstract class Conta {
 	private Cliente titular;
 	private static int total;
 	
+	/**
+	 * Construtor para inicializar o objeto Conta a partir da agencia e numero.
+	 * 
+	 * @param agencia
+	 * @param numero
+	 */
 	public Conta(int agencia, int numero) {
 		Conta.total++;
 		this.agencia = agencia;
@@ -14,7 +26,13 @@ public abstract class Conta {
 	}
 	
 	public abstract void deposita(double valor);
-
+	
+	/**
+	 * Voce precisa ser maior do que o saldo.
+	 * 
+	 * @param valor
+	 * @throws SaldoInsuficienteException
+	 */
 	public void saca(double valor) throws SaldoInsuficienteException {
 		if (this.saldo < valor) {
 			throw new SaldoInsuficienteException("Saldo: " + this.saldo + ", Valor: " + valor);

88d46f5fc93774b5a42c036f4942e9283c993fec
igorfcfs <igorfcfs@gmail.com>
2022-03-16 15:18:11 -0300

    O Pacote java.lang

--- bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteContas.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/test/TesteContas.java
@@ -1,14 +1,13 @@
-package br.com.bytebank.banco.teste;
+package br.com.bytebank.banco.test;
 
 import br.com.bytebank.banco.modelo.ContaCorrente;
 import br.com.bytebank.banco.modelo.ContaPoupanca;
 import br.com.bytebank.banco.modelo.SaldoInsuficienteException;
+//import java.lang.*;
 
 //import br.com.bytebank.banco.modelo.*;
-
-
 public class TesteContas {
-	
+	//java.lang.String
 	public static void main(String[] args) throws SaldoInsuficienteException {
 		//Full Qualified Name (FQN)
 		ContaCorrente cc = new ContaCorrente(111, 111);
@@ -19,6 +18,7 @@ public class TesteContas {
 		
 		cc.tranfere(10.0, cp); //polimorfismo
 		
+		//java.lang.System
 		System.out.println("CC " + cc.getSaldo());
 		System.out.println("CP " + cp.getSaldo());
 				
